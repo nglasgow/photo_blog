4fe38d501f977c8bf366d921ce379abe85b11f8e
Nicole Glasgow <nicole.glasgow@ymail.com>
2013-09-12 23:41:31 -0700

    heroku related changes pt.2

--- config/initializers/carrierwave.rb
+++ config/initializers/carrierwave.rb
@@ -10,4 +10,5 @@ CarrierWave.configure do |config|
 
   config.fog_directory = ENV["AWS_S3_BUCKET"]
   config.fog_public = false
+  config.fog_attributes = {'Cache-Control'=>'max-age=315576000'}
 end
